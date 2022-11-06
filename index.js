1e6437573e89f9527e2a9a44b0ad21292d4b8d23
Sam Siggs <samsiggs00@gmail.com>
2022-11-06 18:41:19 +1300

    Removed logs

--- index.js
+++ index.js
@@ -15,24 +15,9 @@ async function get_stuff(idCounter) {
   });
 }
 
-var isInViewport = function (elem) {
-  var bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-
 let idCounter = 0;
 onload = async (event) => {
-  console.log('Loadded');
   await get_stuff(idCounter);
-  document.getElementById('slides-container').addEventListener('scroll', function(event) {
-    console.log('scrolled');
-  });
 };
 
 
