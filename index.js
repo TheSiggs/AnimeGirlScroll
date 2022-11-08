def3fe43f84e9a5412cb5eadc73195c7723ce0a5
Sam Siggs <samsiggs00@gmail.com>
2022-11-08 18:03:32 +1300

    Added constant loading

--- index.js
+++ index.js
@@ -14,9 +14,28 @@ async function get_stuff(idCounter) {
   });
 }
 
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 let idCounter = 0;
 onload = async (event) => {
   await get_stuff(idCounter);
+  document.getElementById('slides-container').onmouseover = async () => {
+    const ele = document.getElementById('' + idCounter + 28);
+    if (isInViewport(ele)) {
+      // the desired place
+      idCounter++;
+      await get_stuff(idCounter);
+    }
+  }
 };
 
 
